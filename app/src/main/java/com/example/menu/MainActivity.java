bb4ae2d6a35cff1c68025fdacad72eb37ea48e91
Ibrányi Levente <ibranyilevente@gmail.com>
2021-11-18 16:18:31 +0100

    activity_main2.xml done

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonNev, buttonInformacio
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        buttonNev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Egyik odaról a másikra váltás => Intent
+            }
+        });
+    }
+
+    private void init(){
+        buttonNev = findViewById(R.id.button_nev);
+        buttonInformacio = findViewById(R.id.button_informacio);
     }
 }
